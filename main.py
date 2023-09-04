bd45c96105b750866c3dae2e74a16f841a15ca4a
Quynn Bell <quynnbell@arizona.edu>
2023-09-03 20:13:11 -0700

    implementing initial row_reduction algorithm

--- main.py
+++ main.py
@@ -10,32 +10,67 @@
 #   Author: Quynn Bell
 #
 
-import numpy as np 
-from sympy import Matrix
+import numpy as np
 
-rng = np.random.default_rng()
+def row_reduction(matrix):
+    """
+    This function performs row reduction on a given matrix to convert it to row-echelon form.
 
-def row_echelon(A):
-    # get the number of rows and columns of A to use in for loops
-    r, c = A.shape
-    # perform row reduction on matrix from second row
-    for j in range(c):
-        for i in range(j+1, r):
-            if A[j,j] != 0.0:
-                ratio = A[i,j]/A[j,j]
-                A[i] = A[i] - ratio * A[j]
+    Args:
+        matrix (numpy.ndarray): The input matrix as a NumPy array.
 
-    return A
+    Returns:
+        matrix (numpy.ndarray): The row-echelon form of the input matrix.
+    """
+
+    # Convert the matrix to floating-point integers
+    matrix = matrix.astype(float)
+    # use shape to get rows and columns of the matrix
+    m, n = matrix.shape
+    r = 0  # Row index
+    for c in range(n):
+        # Find the pivot element
+        pivot_row = r
+        while pivot_row < m and matrix[pivot_row, c] == 0:
+            pivot_row += 1
+
+        if pivot_row == m:
+            # No pivot element found in this column, move to the next column
+            continue
+
+        # Swap rows to make the pivot element the leading 1
+        matrix[[r, pivot_row]] = matrix[[pivot_row, r]]
+
+        # Scale the row to make the pivot element equal to 1
+        matrix[r, :] /= matrix[r, c]
+
+        # Eliminate other entries in the current column
+        for i in range(m):
+            if i != r:
+                matrix[i, :] -= matrix[i, c] * matrix[r, :]
+
+        r += 1
+
+    threshold = 1e-10
+    matrix[np.abs(matrix) < threshold] = 0
+
+    return matrix
+
 
 def test_case():
-    for i in range(10**2):
-        m=rng.integers(-3,3,size=(3,3))
-        efm=row_echelon(m)
-        #To test whether or not your program output is in echelon form, convert the array to type Matrix from sympy library, and check attribute is_echelon
-        test=Matrix(efm).is_echelon
-        if test == False:
-                print(m)
-                print(efm)
+    # Define test case array
+    A = np.array([[12, -7, 11, -9, 5],
+              [-9, 4, -8, 7, -6],
+              [-6, 11, -7, 3, -9],
+              [4, -6, 10, -5, 12]])
+    
+    # Pass array into row_reduction function
+    row_echelon_form = row_reduction(A)
+
+    # Display output in terminal
+    print("Row-Echelon Form:")
+    print(row_echelon_form)
+
 
 if __name__ == '__main__':
     test_case()
